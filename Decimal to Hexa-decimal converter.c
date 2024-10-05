66454a9e01236ef3bddacd4c19461f654f43e49c
Rasel Hasan <55950753+RH1010@users.noreply.github.com>
2024-10-05 19:00:13 +0600

    Add files via upload

---
+++ Decimal to Hexa-decimal converter.c
@@ -0,0 +1,13 @@
+//Program name: Decimal to Hexa-decimal converter
+
+#include<stdio.h>
+int main()
+{
+    int number;
+
+    printf("Please enter your Decimal number: ");
+    scanf("%d",&number);
+    printf("Hexa-decimal number is: %x",number);
+
+    return 0;
+}
---
+++ Decimal to Octal converter.c
@@ -0,0 +1,13 @@
+//Program name: Decimal to Octal converter
+
+#include<stdio.h>
+int main()
+{
+    int number;
+
+    printf("Please enter your Decimal number: ");
+    scanf("%d",&number);
+    printf("Octal number is: %o",number);
+
+    return 0;
+}
