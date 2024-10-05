a57d2bbb19303623829ea5d1edb1aaac622091a7
Rasel Hasan <55950753+RH1010@users.noreply.github.com>
2024-10-05 18:57:46 +0600

    Add files via upload

---
+++ Calculator.c
@@ -0,0 +1,34 @@
+// Program name: Calculator
+#include <stdio.h>
+
+int main() {
+
+  char op;
+  double first, second;
+
+  printf("\t\t\t\t\tWelcome to Calculator\n");
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    // operator doesn't match any case constant
+    default:
+      printf("Error! operator is not correct");
+  }
+
+  return 0;
+}
