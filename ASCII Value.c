436d8fcba350fb7fe59f035ae5c4f792b2c9c526
RH1010 <55950753+RH1010@users.noreply.github.com>
2024-10-05 18:49:38 +0600

    Add files via upload

---
+++ ASCII Value.c
@@ -0,0 +1,28 @@
+//Program name: ASCII Value
+/*
+#include<stdio.h>
+int main()
+{
+    int n;
+
+    printf("Enter any ascii value: ");
+    scanf("%d",&n);
+    printf("Ascii character is: %c",n);
+
+    return 0;
+}
+*/
+//or
+/*
+#include<stdio.h>
+int main()
+{
+    int ch;
+
+    printf("Ascii character is: ");
+    scanf("%c",&ch);
+    printf("Ascii value :  %d",ch);
+
+    return 0;
+}
+*/
